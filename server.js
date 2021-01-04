d703d4785e5ea705df55423d8700512cf9e567ea
somoye123 <somoye.ayotunde@gmail.com>
2021-01-04 19:45:50 +0100

    convert es5 dependencies to es6+ syntax

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
  */
 
 //  Dependencies
-const http = require('http');
-const url = require('url');
+import http from 'http';
+import url from 'url';
 
 // Server respond to all request with a string
 const server = http.createServer(function (req, res) {
