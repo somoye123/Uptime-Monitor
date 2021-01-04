a355ebb801c3164548bc69ac6389470430eabd0c
somoye123 <somoye.ayotunde@gmail.com>
2021-01-04 19:15:05 +0100

    Server respond to all request with a string

--- server.js
+++ server.js
@@ -5,3 +5,8 @@
 //  Dependencies
 const http = require('http');
 const url = require('url');
+
+// Server respond to all request with a string
+const server = http.createServer(function (req, res) {
+  res.end('Hello World|\n');
+});
