624a4d5841c6b287f19139877acb344b3643e999
somoye123 <somoye.ayotunde@gmail.com>
2021-01-08 00:22:34 +0100

    add ping handler function to handler object

--- server.js
+++ server.js
@@ -96,6 +96,9 @@ const unifiedServer = (req, res) => {
 //Define all the handles
 const handlers = {};
 
+// Ping handler
+handlers.ping = (data, callback) => callback(200);
+
 // Not found handler
 handlers.notFound = (data, callback) => callback(404);
 
