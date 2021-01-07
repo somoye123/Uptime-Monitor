03b328809f6201932695284e70841b4f521ef6e7
somoye123 <somoye.ayotunde@gmail.com>
2021-01-08 00:04:12 +0100

    add fs to application dependencies

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import https from 'https';
 import url from 'url';
 import { StringDecoder } from 'string_decoder';
 import config from './config';
+import fs from 'fs';
 
 // Instantiate the HTTP server
 const server = http.createServer((req, res) => unifiedServer(req, res));
