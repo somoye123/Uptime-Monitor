dadef0e79fcaea09465693712b2cda9380ed6be3
somoye123 <somoye.ayotunde@gmail.com>
2021-01-15 14:57:17 +0100

    abstract users get and put logic to users controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -76,4 +76,96 @@ Controller.post = (data, callback) => {
     callback(400, { Error: 'Missing required fields' });
   }
 };
+
+// Required data: phone
+// Optional data: none
+Controller.get = (data, callback) => {
+  // Check that phone number is valid
+  const phone =
+    typeof data.queryStringObject.phone == 'string' &&
+    data.queryStringObject.phone.trim().length == 11
+      ? data.queryStringObject.phone.trim()
+      : false;
+  if (phone) {
+    // Lookup the user
+    _data.read('users', phone, function (err, data) {
+      if (!err && data) {
+        // Remove the hashed password from the user user object before returning it to the requester
+        delete data.hashedPassword;
+        callback(200, data);
+      } else {
+        callback(404);
+      }
+    });
+  } else {
+    callback(400, { Error: 'Missing required field' });
+  }
+};
+
+// Required data: phone
+// Optional data: firstName, lastName, password (at least one must be specified)
+Controller.put = (data, callback) => {
+  // Check for required field
+  const phone =
+    typeof data.payload.phone == 'string' &&
+    data.payload.phone.trim().length == 11
+      ? data.payload.phone.trim()
+      : false;
+
+  // Check for optional fields
+  const firstName =
+    typeof data.payload.firstName == 'string' &&
+    data.payload.firstName.trim().length > 0
+      ? data.payload.firstName.trim()
+      : false;
+  const lastName =
+    typeof data.payload.lastName == 'string' &&
+    data.payload.lastName.trim().length > 0
+      ? data.payload.lastName.trim()
+      : false;
+  const password =
+    typeof data.payload.password == 'string' &&
+    data.payload.password.trim().length > 0
+      ? data.payload.password.trim()
+      : false;
+
+  // Error if phone is invalid
+  if (phone) {
+    // Error if nothing is sent to update
+    if (firstName || lastName || password) {
+      // Lookup the user
+      _data.read('users', phone, (err, userData) => {
+        if (!err && userData) {
+          // Update the fields if necessary
+          if (firstName) {
+            userData.firstName = firstName;
+          }
+          if (lastName) {
+            userData.lastName = lastName;
+          }
+          if (password) {
+            userData.hashedPassword = helpers.hash(password);
+          }
+          // Store the new updates
+          _data.update('users', phone, userData, function (err) {
+            if (!err) {
+              // Remove the hashed password from the user user object before returning it to the requester
+              delete userData.hashedPassword;
+              callback(200, userData);
+            } else {
+              console.log(err);
+              callback(500, { Error: 'Could not update the user.' });
+            }
+          });
+        } else {
+          callback(400, { Error: 'Specified user does not exist.' });
+        }
+      });
+    } else {
+      callback(400, { Error: 'Missing fields to update.' });
+    }
+  } else {
+    callback(400, { Error: 'Missing required field.' });
+  }
+};
 export default Controller;
