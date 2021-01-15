6996a971c67143dc5783cef0dbb4a8798ebaabc1
somoye123 <somoye.ayotunde@gmail.com>
2021-01-15 15:33:45 +0100

    build token controller get method

--- controllers/tokensController.js
+++ controllers/tokensController.js
@@ -57,4 +57,28 @@ Controller.post = (data, callback) => {
   }
 };
 
+// Tokens - get
+// Required data: id
+// Optional data: none
+handlers._tokens.get = (data, callback) => {
+  // Check that id is valid
+  const id =
+    typeof data.queryStringObject.id == 'string' &&
+    data.queryStringObject.id.trim().length == 20
+      ? data.queryStringObject.id.trim()
+      : false;
+  if (id) {
+    // Lookup the token
+    _data.read('tokens', id, function (err, tokenData) {
+      if (!err && tokenData) {
+        callback(200, tokenData);
+      } else {
+        callback(404);
+      }
+    });
+  } else {
+    callback(400, { Error: 'Missing required field, or field invalid' });
+  }
+};
+
 export default Controller;
