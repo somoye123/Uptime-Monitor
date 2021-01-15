e09163abfe3797e0df0b9365b2d144cfd70e2941
somoye123 <somoye.ayotunde@gmail.com>
2021-01-15 15:29:59 +0100

    build token controller post method

--- controllers/tokensController.js
+++ controllers/tokensController.js
@@ -4,4 +4,57 @@ import helpers from '../lib/helpers';
 
 const Controller = {};
 
+// Tokens - post
+// Required data: phone, password
+// Optional data: none
+Controller.post = (data, callback) => {
+  const phone =
+    typeof data.payload.phone == 'string' &&
+    data.payload.phone.trim().length == 11
+      ? data.payload.phone.trim()
+      : false;
+  const password =
+    typeof data.payload.password == 'string' &&
+    data.payload.password.trim().length > 0
+      ? data.payload.password.trim()
+      : false;
+  if (phone && password) {
+    // Lookup the user who matches that phone number
+    _data.read('users', phone, (err, userData) => {
+      if (!err && userData) {
+        // Hash the sent password, and compare it to the password stored in the user object
+        const hashedPassword = helpers.hash(password);
+        if (hashedPassword == userData.hashedPassword) {
+          // If valid, create a new token with a random name. Set an expiration date 1 hour in the future.
+          const tokenId = helpers.createRandomString(20);
+          const expires = Date.now() + 1000 * 60 * 60;
+          const tokenObject = {
+            phone,
+            id: tokenId,
+            expires,
+          };
+
+          // Store the token
+          _data.create('tokens', tokenId, tokenObject, (err) => {
+            if (!err) {
+              callback(200, tokenObject);
+            } else {
+              callback(500, { Error: 'Could not create the new token' });
+            }
+          });
+        } else {
+          callback(400, {
+            Error:
+              "Password did not match the specified user's stored password",
+          });
+        }
+      } else {
+        callback(400, { Error: 'Could not find the specified user.' });
+      }
+    });
+  } else {
+    callback(400, { Error: 'Missing required field(s).' });
+  }
+};
+
 export default Controller;
