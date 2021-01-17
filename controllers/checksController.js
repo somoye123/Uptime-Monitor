82277bafc7c52b3cc30cbad4513e2480a445b9e4
somoye123 <somoye.ayotunde@gmail.com>
2021-01-17 18:49:28 +0100

    add verify token dependency

--- controllers/checksController.js
+++ controllers/checksController.js
@@ -2,6 +2,7 @@
 import _data from '../lib/data';
 import helpers from '../lib/helpers';
 import config from '../lib/config';
+import { verifyToken } from './tokensController';
 
 const Controller = {};
 
